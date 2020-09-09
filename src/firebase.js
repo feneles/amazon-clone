3c11268c6c139d832e25bc4d54425437454bdcb6
Marek Rogala <rogala.marek91@gmail.com>
2020-09-09 12:17:35 +0200

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBENmNprVoiyA1tGKYOdD0duXmqF-cXC1k",
+  authDomain: "clone-57744.firebaseapp.com",
+  databaseURL: "https://clone-57744.firebaseio.com",
+  projectId: "clone-57744",
+  storageBucket: "clone-57744.appspot.com",
+  messagingSenderId: "891852579040",
+  appId: "1:891852579040:web:a0a8e74138036403d9b085",
+  measurementId: "G-HC9VPVT38S",
+};
